a26043cae692c9f96704602db7e093b4e855753f
Felipe Salles Lopes <felipe28101997@gmail.com>
2019-11-15 00:15:07 -0400

    Formatação de mensagem

--- Trabalho/src/trabalho/Client.java
+++ Trabalho/src/trabalho/Client.java
@@ -58,15 +58,15 @@ public class Client {
 
 			bw.write(sendMessage);
 			bw.flush();
-
-			System.out.println("Mensagem enviada ao servidor: " + sendMessage);
+			
+			System.out.println("\nMensagem enviada ao servidor: " + sendMessage + "\n");
 
 			// Recebe a mensagem de retorno do servidor.
 			InputStream in = socket.getInputStream();
 			InputStreamReader inReader = new InputStreamReader(in);
 			BufferedReader br = new BufferedReader(inReader);
 			String message = br.readLine();
-			System.out.println("\n" + message);
+			System.out.println(message);
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		} finally {
