410340d308e23e65fb534bd6e2a05638cb47e51e
Felipe Salles Lopes <felipe28101997@gmail.com>
2019-11-24 03:34:02 -0400

    Adição de classe dedicada ao gerenciamento de diretórios

---
+++ Trabalho/src/trabalho/FileManagement.java
@@ -0,0 +1,36 @@
+package trabalho;
+
+import java.io.File;
+
+public class FileManagement {
+	private String dirs[];
+	/*
+	 * private String path;
+	 * 
+	 * public FileManagement(String path) { this.path = path; }
+	 */
+	
+	boolean createDir(String path) {
+		
+		path = path.replace("\\", "/");
+		dirs = path.split("/");
+
+		return createDir(dirs[0], 0);
+		
+	}
+	
+	private boolean createDir(String path, int i) {
+		
+		File file = new File(path);		
+		boolean fileCreated = false;
+		
+		if (!file.exists())
+			fileCreated = file.mkdir();	
+		
+		if (i < dirs.length) {
+			return createDir(path + "\\" + dirs[++i], i);
+		}
+		
+		return true;
+	}
+}
